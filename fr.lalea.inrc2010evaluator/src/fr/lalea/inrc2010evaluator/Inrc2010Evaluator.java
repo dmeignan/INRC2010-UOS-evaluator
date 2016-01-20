2dc3004666cf7059bd9b2c3bf9efc44c71fa068b
David Meignan <dmeignan@vidar.informatik.uni-osnabrueck.de>
2016-01-20 16:47:00 +0100

    Initial commit

---
+++ fr.lalea.inrc2010evaluator/src/fr/lalea/inrc2010evaluator/Inrc2010Evaluator.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2016, Meignan Consulting (Deutschland)
+ * Author: David Meignan
+ */
+package fr.lalea.inrc2010evaluator;
+
+/**
+ * @author David Meignan
+ *
+ */
+public class Inrc2010Evaluator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
